aed69c0e4726a4ef07a366373f826ac32d5a7e81
liuzheng-arctic <liuzheng.atmos@gmail.com>
2023-12-01 23:53:33 -0800

    move things around and add code to handle daily txt data and single netcdf bodata

---
+++ bopak/__init__.py
@@ -0,0 +1,2 @@
+from .bodata import *
+from .reprocess import set_BOEASE2
---
+++ bopak/reprocess/scripts/combine_gridded.py
@@ -0,0 +1,39 @@
+import numpy as np
+import xarray as xr 
+import pandas as pd
+from pathlib import Path
+
+from bopak.reprocess import combined_data as cbd
+
+
+
+BOROOT = Path('/data/BO/EASE2')
+
+
+ftype = 'nc'
+freq = 'Y'
+outroot = BOROOT/'combined' 
+SYEAR = 2007
+EYEAR = 2007
+
+
+albdir = BOROOT/'albedo/V0.05'
+sicdir = BOROOT/'SIC'
+onsetdir = BOROOT/'onset'
+iadir = BOROOT/'ICEAGE/V4'
+eradir = BOROOT/'ERA5/'
+piodir = BOROOT/'PIOMAS'
+ponddir = BOROOT/'pond'
+
+for tyr in range(SYEAR, EYEAR+1):
+    tdat = cbd(
+        tyr,
+        albdir=albdir,
+        sicdir=sicdir,
+        onsetdir=onsetdir,
+        iadir=iadir, 
+        eradir=eradir,
+        piodir=piodir,
+        ponddir=ponddir)
+    tdat.process()
+    #tdat.to_file(outroot,ftype=ftype,freq=freq)
